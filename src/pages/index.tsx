3e9dd70e1adcbffac1de5c52804ce4cb47316646
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-16 16:34:44 -0400

    added header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import { Button, Switch } from '@mui/material'
+import Head from 'next/head'
+import Header from '../components/nav/Header'
 
 export default function Home() {
 	return (
-		<div>
-			hello world
-			<Switch />
-			<Button color="primary">
-				hello
-			</Button>
-		</div>
+		<>
+			<Head>
+				<title>GT Marine Robotics Group</title>
+			</Head>
+			<div>
+				<Header />
+				hello world
+				<Switch />
+				<Button color="primary">
+					hello
+				</Button>
+			</div>
+		</>
 	)
 }
