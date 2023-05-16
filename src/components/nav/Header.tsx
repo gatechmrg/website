bfc2bd0f7b2e47463920c487490bfc609138800f
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-16 16:34:44 -0400

    added header

---
+++ src/components/nav/Header.tsx
@@ -0,0 +1,39 @@
+import { AppBar, Box, Grid, Typography } from "@mui/material";
+import Link from "next/link";
+
+const pages = [
+    {name: 'About', link: '/about'},
+    {name: 'Projects', link: '/projects'},
+    {name: 'Sponsors', link: '/sponsors'},
+    {name: 'Join Us', link: '/join-us'}
+]
+
+export default function Header() {
+
+    return (
+        <AppBar position="sticky" sx={{bgcolor: "background.default"}}>
+            <Box mx={3} py={1}>
+                <Grid container alignItems="center">
+                    <Grid item>
+                        <Box>
+                            <img src="/mrg_logo.png" alt="MRG Logo"
+                                height={60} />
+                        </Box>
+                    </Grid>
+                    <Grid item flex={1} />
+                    {pages.map(page => (
+                        <Grid item key={page.name}>
+                            <Link href={page.link}>
+                                <Box ml={5} sx={{':hover': {color: "primary.main"}}}>
+                                    <Typography variant="body1" sx={{fontSize: '1.2rem'}}>
+                                        {page.name}
+                                    </Typography>
+                                </Box>
+                            </Link>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </AppBar>
+    )
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,10 @@ export const theme = createTheme({
     palette: {
         primary: {
             main: 'hsl(166, 100%, 35%)'
+        },
+        background: {
+            default: 'hsl(166, 98%, 98%)',
+            paper: '#fff'
         }
     }
 })
