2d9bcac4999aed742bb08fff3b880a6fbc93a7b1
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-18 22:12:22 -0400

    small screen header
    Former-commit-id: 89916f0e2911316830932322de461f2bd3130891

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,5 +1,8 @@
-import { AppBar, Box, Grid, Typography } from "@mui/material";
+import { AppBar, Box, Drawer, Grid, IconButton, List, ListItem, Typography, useMediaQuery } from "@mui/material";
 import Link from "next/link";
+import { useState } from "react";
+import { useTheme } from '@mui/material/styles'
+import MenuIcon from "@mui/icons-material/Menu"
 
 const pages = [
     {name: 'About', link: '/about'},
@@ -9,6 +12,11 @@ const pages = [
 ]
 
 export default function Header() {
+
+    const theme = useTheme()
+    const mdUp = useMediaQuery(theme.breakpoints.up('md'))
+
+    const [openDrawer, setOpenDrawer] = useState(false)
 
     return (
         <AppBar position="sticky" sx={{bgcolor: "background.default"}}>
@@ -23,19 +31,43 @@ export default function Header() {
                         </Link>
                     </Grid>
                     <Grid item flex={1} />
-                    {pages.map(page => (
-                        <Grid item key={page.name}>
-                            <Link href={page.link}>
-                                <Box ml={5} sx={{':hover': {color: "primary.main"}}}>
-                                    <Typography variant="body1" sx={{fontSize: '1.2rem'}}>
-                                        {page.name}
-                                    </Typography>
-                                </Box>
-                            </Link>
+                    {mdUp ? 
+                        pages.map(page => (
+                            <Grid item key={page.name}>
+                                <Link href={page.link}>
+                                    <Box ml={5} sx={{':hover': {color: "primary.main"}}}>
+                                        <Typography variant="body1" sx={{fontSize: '1.2rem'}}>
+                                            {page.name}
+                                        </Typography>
+                                    </Box>
+                                </Link>
+                            </Grid>
+                        ))
+                        :
+                        <Grid item>
+                            <IconButton onClick={() => setOpenDrawer(true)}>
+                                <MenuIcon fontSize="large" />
+                            </IconButton>
                         </Grid>
-                    ))}
+                    }
                 </Grid>
             </Box>
+             <Drawer anchor="left" open={openDrawer}
+                    onClose={() => setOpenDrawer(false)}>
+                    <Box>
+                        <List>
+                            {pages.map(page => (
+                                <Link href={page.link}>
+                                    <ListItem sx={{width: 200, cursor: 'pointer', mb: 2}}>
+                                        <Typography variant="body1" sx={{fontSize: '1.2rem'}}>
+                                            {page.name}
+                                        </Typography>
+                                    </ListItem>
+                                </Link>
+                            ))}
+                        </List>
+                    </Box>
+                </Drawer>
         </AppBar>
     )
 }
