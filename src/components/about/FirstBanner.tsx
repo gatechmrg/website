f7e7f53baf463c66100f9969ae1e5282631206be
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-21 01:16:54 -0400

    about banner for small screens
    Former-commit-id: 91fffbe5697dee1c492b3069fb0aecb3cbaeb245

--- src/components/about/FirstBanner.tsx
+++ src/components/about/FirstBanner.tsx
@@ -1,6 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { useTheme } from '@mui/material/styles'
 
 export default function FirstBanner() {
+
+    const theme = useTheme()
+    const small = useMediaQuery(theme.breakpoints.down('md'))
 
     return (
         <>
@@ -12,8 +16,8 @@ export default function FirstBanner() {
                 </defs>
             </svg>
             <Box sx={{
-                backgroundImage: 'url(/about/people.JPG)',
-                height: 600,
+                backgroundImage: small ? 'none' : 'url(/about/people.JPG)',
+                height: small ? 200 : 600,
                 backgroundPosition: 'right',
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
@@ -21,11 +25,12 @@ export default function FirstBanner() {
             }}>
                 <Box position="absolute" top={0} left={0} width="calc(50vw - 600px + 60px)" height={600} 
                     bgcolor="background.default" zIndex={1} />
-                <Box overflow="hidden" height={600} bgcolor="background.default" sx={{
+                <Box overflow="hidden" height={small ? 200 : 600} bgcolor="background.default" sx={{
                     clipPath: 'url(#about-clip-path)'
                 }} maxWidth="lg" mx="auto" display="grid" alignItems="center"
                 position="relative" zIndex={2}>
-                    <Typography sx={{marginTop: '0px', marginLeft: 5}} variant="h1">
+                    <Typography sx={{marginTop: '0px', marginLeft: small ? 3 : 5}} 
+                        variant={small ? 'h2' : 'h1'}>
                         About Us
                     </Typography>
                 </Box>
