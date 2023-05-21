0c9ca83909fa450d16f613b92addfd072349ad19
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-21 00:42:21 -0400

    added large screen pictures

---
+++ src/components/about/Main.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import FirstBanner from "./FirstBanner";
+import About from "./About";
+
+export default function Main() {
+
+    return (
+        <Box>
+            <FirstBanner />
+            <About />
+        </Box>
+    )
+}
