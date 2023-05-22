604eadc1e80f851bfe0e7466da971d2b13b49567
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-22 17:20:34 -0400

    first roboboat banner
    Former-commit-id: ce411fb669273259db53e606444e7726844fd0e9

---
+++ src/components/projects/roboboat/FirstBanner.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { useTheme } from '@mui/material/styles'
+
+export default function FirstBanner() {
+
+    const theme = useTheme() 
+    const small = useMediaQuery(theme.breakpoints.down('sm'))
+
+    return (
+        <Box sx={{
+            backgroundImage: 'url(/projects/roboboat/at-comp2.png)',
+            height: 800,
+            backgroundPosition: 'bottom',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            position: 'relative',
+        }}>
+            <Box height="100%" display="flex" flexDirection="column">
+                <Box flex={1} />
+                <Box textAlign="center" py={5} bgcolor="rgba(0,0,0,.5)">
+                    <Typography variant="h1" fontWeight={600} color="#fff"
+                    fontSize={small ? '5rem' : '8rem'}
+                    sx={{textShadow: "1px 1px 3px #000"}}>
+                        RoboBoat
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
