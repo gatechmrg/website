1e16c4bb2f7faa87a36387a0485cf3c95deab4f4
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-23 17:08:08 -0400

    robotx team roster
    Former-commit-id: 901bcf311505789497e252289939cd680e4eb7e0

--- src/components/projects/robotx/Main.tsx
+++ src/components/projects/robotx/Main.tsx
@@ -6,6 +6,7 @@ import UAV from "./UAV";
 import Mechanical from "./Mechanical";
 import Electrical from "./Electrical";
 import Software from "./Software";
+import TeamRoster from "./TeamRoster";
 
 export default function Main() {
 
@@ -18,6 +19,7 @@ export default function Main() {
             <Mechanical />
             <Electrical />
             <Software />
+            <TeamRoster />
         </Box>
     )
 }
