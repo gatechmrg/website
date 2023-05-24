49418d157951ca0f133bcccf179e1c29186199a0
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-24 11:22:25 -0700

    robosub auv section
    Former-commit-id: 361600cfdc2e1c28a2bc6de3b9b34ab6abcc8088

--- src/components/projects/robosub/Main.tsx
+++ src/components/projects/robosub/Main.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import FirstBanner from "./FirstBanner";
 import Competition from "./Competition";
+import AUV from "./AUV";
 
 export default function Main() {
 
@@ -8,6 +9,7 @@ export default function Main() {
         <Box>
             <FirstBanner />
             <Competition />
+            <AUV />
         </Box>
     )
 }
