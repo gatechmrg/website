f1765c97c5cda6217a37ba12391d767240b39c54
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-24 18:18:04 -0700

    microtransat video

--- src/components/projects/microtransat/Main.tsx
+++ src/components/projects/microtransat/Main.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import FirstBanner from "./FirstBanner";
+import Challenge from "./Challenge";
 
 export default function Main() {
 
     return (
         <Box>
             <FirstBanner />
+            <Challenge />
         </Box>
     )
 }
