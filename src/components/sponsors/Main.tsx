ff2d19a36a45b47970f9c3abc4dfe42a4464d085
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-25 18:43:00 -0700

    sponsors page
    Former-commit-id: 16a7a3f8070beb9e85645e2c9eca8cb91e3a3d70

---
+++ src/components/sponsors/Main.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import Current from "./Current";
+
+export default function Main() {
+    
+    return (
+        <Box>
+            <Current />
+        </Box>
+    )
+}
