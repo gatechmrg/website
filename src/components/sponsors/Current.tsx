b92c94ece2212af51357c801bb3c7aaa0adb8b40
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-25 18:43:00 -0700

    sponsors page
    Former-commit-id: 34b23f2d43e47ea20455f6b637dd658b905b36c0

---
+++ src/components/sponsors/Current.tsx
@@ -0,0 +1,53 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { StandardLine } from "../misc/line";
+
+export default function Current() {
+
+    return (
+        <Box mt={6}>
+            <Container maxWidth="xl">
+                <Box textAlign="center">
+                    <Typography variant="h2">
+                        Current Sponsors
+                    </Typography>
+                </Box>
+                <Box mt={2} mb={5}>
+                    <StandardLine width={200} height={5} centered borderRadius={3} />
+                </Box>
+                <Box>
+                    <Grid container spacing={8} alignItems="center"
+                        justifyContent="center">
+                        <Grid item>
+                            <img src="/sponsors/asdl.png" 
+                                style={{maxWidth: 500, maxHeight: 500}} />
+                        </Grid>
+                        <Grid item>
+                            <img src="/sponsors/sga.png" 
+                                style={{maxWidth: 500, maxHeight: 500}} />
+                        </Grid>
+                        <Grid item>
+                            <img src="/sponsors/altium.png" 
+                                style={{maxWidth: 500, maxHeight: 500}} />
+                        </Grid>
+                        <Grid item>
+                            <img src="/sponsors/greenzie.png" 
+                                style={{maxWidth: 500, maxHeight: 500}} />
+                        </Grid>
+                    </Grid>
+                </Box>
+                <Box mt={10} mb={5}>
+                    <StandardLine width={200} height={5} centered borderRadius={3} />
+                </Box>
+                <Box maxWidth={620} textAlign="center" mx="auto">
+                    <Typography variant="body1" fontSize="1.2rem">
+                        If you are interested in sponsoring our organization, please
+                        email us at
+                    </Typography>
+                    <Typography variant="body1" fontSize="1.2rem">
+                        marinerobotics@groups.gatech.edu
+                    </Typography>
+                </Box>
+            </Container>
+        </Box>
+    )
+}
