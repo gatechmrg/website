3b9a400ddf123e74e0c386f84c53ec7592b47880
Manuel Roglan <manueljoseph113@gmail.com>
2023-06-24 18:02:56 -0400

    added meta description for about page
    Former-commit-id: 5e3cc8f24949f3f3431e3ec0fe79c8b644651a0d

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,6 +9,9 @@ export default function About() {
         <>
             <Head>
                 <title>About | GT Marine Robotics Group</title>     
+                <meta name="description"
+                    content="About the Marine Robotics Group at Georgia Tech.
+                    Learn about the organization and its leadership." />
             </Head> 
             <div className="root-header-footer">
                 <Header />
