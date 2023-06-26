ca25f06d6a65956726fb2fdc7eac28b9a3c9d2ab
Manuel Roglan <manueljoseph113@gmail.com>
2023-06-25 20:55:47 -0400

    using webp on roboboat page
    Former-commit-id: 01df1af57a5c55a06ceb29e261dcc07cac216bf2

--- src/components/projects/roboboat/Competition.tsx
+++ src/components/projects/roboboat/Competition.tsx
@@ -16,7 +16,7 @@ export default function Competition() {
                 </Box>
                 <Grid container justifyContent="center">
                     <Grid item xs={12} sm={8} md={6}>
-                        <img width="100%" src="/projects/roboboat/roboboat-watershooter.png"
+                        <img width="100%" src="/projects/roboboat/roboboat-watershooter.webp"
                         style={{borderRadius: 16}} />
                     </Grid>
                     <Grid item xs={12} md={6}>
