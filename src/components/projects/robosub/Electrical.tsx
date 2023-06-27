34fd4cf2deebcbaaa84b0c1afe141e19b87b5abe
Manuel Roglan <manueljoseph113@gmail.com>
2023-06-26 20:14:05 -0400

    optimize images for robosub page

--- src/components/projects/robosub/Electrical.tsx
+++ src/components/projects/robosub/Electrical.tsx
@@ -11,7 +11,7 @@ export default function Electrical() {
                     <Grid container>
                         <Grid item xs={0} sm={2} md={3} lg={4}>
                             <Box width="100%" height="100%" sx={{
-                                backgroundImage: 'url(/projects/robosub/electrical.PNG)',
+                                backgroundImage: 'url(/projects/robosub/electrical.webp)',
                                 backgroundPosition: 'center',
                                 backgroundRepeat: 'no-repeat',
                                 backgroundSize: 'cover'
