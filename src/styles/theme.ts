cdb7fe2f60769ca0c4ecdece92a5f45482b51385
chxtio <chxtio@gmail.com>
2025-06-18 20:42:47 -0700

    Update theme.ts to match logo colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,23 @@
 import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
-    palette: {
-        primary: {
-            main: 'hsl(166, 100%, 35%)'
-        },
-        background: {
-            default: 'hsl(166, 98%, 98%)',
-            paper: '#fff'
+  palette: {
+    primary: {
+        main: '#00000'
+    },
+    background: {
+      default: '#001848', // dark navy blue 
+      paper: '#C2AC6D'    // Gold modal
+    },
+    text: {
+          primary: '#ffffff', // white text on Gold
+        // primary: '#00000'
+    }
+    },
+    typography: {
+        fontWeightRegular: 500, 
+        allVariants: {
+            fontWeight: 700 // default bold
         }
     }
 })
