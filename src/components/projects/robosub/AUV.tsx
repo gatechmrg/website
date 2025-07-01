e282c3a4c91b6a5426668ada5aec18e48c720dae
erinbeaz <erinbeaz@gmail.com>
2025-06-30 19:53:51 -0700

    Updated RoboSub Project information
    Former-commit-id: b7157b99c5d3ae8aae6bc463ed33ae55b86c8bd4

--- src/components/projects/robosub/AUV.tsx
+++ src/components/projects/robosub/AUV.tsx
@@ -8,7 +8,7 @@ export default function AUV() {
             <Container maxWidth="xl">
                 <Box textAlign="center">
                     <Typography variant="h2">
-                        AUV 2024
+                        AUV 2025
                     </Typography>
                 </Box>
                 <Box mt={2} mb={5}>
@@ -16,8 +16,22 @@ export default function AUV() {
                 </Box>
                 <Grid container justifyContent="center">
                     <Grid item xs={12} sm={8} md={6}>
-                        <img width="100%" src="/projects/robosub/shawnsub.webp"
-                        style={{borderRadius: 16}} alt="MRG's Plongeur being tested at the CRC" />
+                         <Box sx={{ position: "relative", paddingTop: "56.25%", borderRadius: 2, overflow: "hidden" }}>
+                            <iframe
+                                src="https://www.youtube.com/embed/3EeeeBVkHmo"
+                                title="RoboSub Prequal Video"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                style={{
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    width: "100%",
+                                    height: "100%",
+                                    border: 0,
+                                }}
+                            />
+                        </Box>
                     </Grid>
                     <Grid item xs={12} md={6}>
                         <Box mx={3} mt={{xs: 3, md: 0}}>
@@ -25,18 +39,18 @@ export default function AUV() {
                                 <Box>
                                     <Typography variant="body1" textAlign={{xs: 'center', md: 'left'}}
                                         lineHeight={1.7} fontSize="1.2rem">
-                                        We will be constructing a new vehicle for 2024, which
-                                        does not yet have a name! Testing will take place in the CRC.
+                                        A new vehicle, Take Two, will be making it's competition debut at RoboSub 2025 for MRG's first return since 2020.
+                                        The platform focuses on simplicity and modularity, enabeling strong future development.
                                     </Typography>
                                 </Box>
                                 <Box mt={2}>
                                     <Typography variant="body1" textAlign={{xs: 'center', md: 'left'}}
                                         lineHeight={1.7} fontSize="1.2rem">
-                                        We plan on learning from our previous RoboSub vehicle (from 2019) as we tackle
-                                        the building and programming of the new vehicle. Upcoming projects 
-                                        include designing of main platform, actuator development, 
-                                        perception development, navigation development, and electrical system 
-                                        construction.
+                                Take Two has a cast acrylic hull with eight polycarbonate guide rods around it, where we can mount different subsystems
+                                 and sensors—like the powertrain and other electronics. Inside, and extendable drawer-style setup makes it easy to access
+                                  and work on the electrical components, which include in-house-developed PCBs, fuses, buck converters, 
+                                  and more. We also put a lot of effort into making sure mechanical and software teams 
+                                  could work in parallel by using a medium-fidelity simulation in Gazebo. 
                                     </Typography>
                                 </Box>
                             </Box>
