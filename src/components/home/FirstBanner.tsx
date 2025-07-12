6d630c5666840595c49d250c08ae2d996fb78a41
chxtio <chxtio@gmail.com>
2025-07-12 01:10:52 -0700

    Replace homepage Wam-V pic with autoplay video

--- src/components/home/FirstBanner.tsx
+++ src/components/home/FirstBanner.tsx
@@ -2,30 +2,36 @@ import { Box, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
 import { GreenPrimaryButton } from "../misc/buttons";
 import { useTheme } from '@mui/material/styles'
 import Link from "next/link";
-
 export default function FirstBanner() {
-
     const theme = useTheme()
     const lg = useMediaQuery(theme.breakpoints.up('md'))
     const sm = useMediaQuery(theme.breakpoints.down('md'))
-
     return (
-        <Box sx={{
-            backgroundImage: sm ? 'url(/home/wamv_banner_small.webp)' : lg ? 'url(/home/wamv_banner_large.webp)' : '',
-            height: 800,
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            position: 'relative',
-        }}>
-            <img src={sm ? '/home/wamv_banner_small.webp' : lg ? '/home/wamv_banner_large.webp' : ''}
-                style={{display: 'none'}} width={0} height={0} alt="Dummy background image to prioritize loading" />
-            <Grid container wrap="nowrap" height="100%">
-                <Grid item xl={6} lg={4} md={2} />
+        <Box position={"relative"} width="100%" height="100vh" overflow="hidden">
+            {/* <img src={sm ? '/home/wamv_banner_small.webp' : lg ? '/home/wamv_banner_large.webp' : ''}
+                style={{display: 'none'}} width={0} height={0} alt="Dummy background image to prioritize loading" /> */}
+            <Box
+                component="video"
+                src="/home/hero-video.mp4"
+                autoPlay
+                muted
+                loop
+                playsInline
+                sx={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                    objectFit: "cover",
+                zIndex: -1,
+                }}
+            />
+            
+            <Grid container wrap="nowrap" height="100%" justifyContent="center" >
+                <Grid item xl={4} lg={2} md={1} />
                 <Grid item flex={1}>
-                    <Box mt={20} textAlign={{xs: 'center', xl: 'left'}}>
+                    <Box mt={40} textAlign={{xs: 'center', xl: 'left'}}>
                         <Box display="inline-block" >
-                            <Typography variant="h1" fontWeight={600} color="#fff"
+                            <Typography variant="h1" fontWeight={500} color="#fff"
                             sx={{textShadow: "1px 1px 3px #000"}}>
                                 Robots set sail!
                             </Typography>
@@ -33,9 +39,9 @@ export default function FirstBanner() {
                             sx={{textShadow: "1px 1px 3px #000"}}>
                                 Tackling the challenges of sending autonomous machines into the blue.
                             </Typography>
-                            <Box mt={2} textAlign="center">
+                            <Box mt={2} textAlign="center" >
                                 <Link href="/join-us">
-                                    <GreenPrimaryButton sx={{minWidth: 200}}>
+                                    <GreenPrimaryButton sx={{minWidth: 150}}>
                                         Join Us
                                     </GreenPrimaryButton>
                                 </Link>
