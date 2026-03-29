908540af957a7d5fe0e699434bc07d95f9f82ac5
Sean Fish <seantfish@gmail.com>
2026-03-29 05:45:21 -0400

    removed notion wiki due to forbidden embedding
    Former-commit-id: 91f97afb3c342a401276e1b7769d8833881d88fb

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -8,7 +8,6 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 const pages = [
     {name: 'About', link: '/about'},
-    {name: 'Wiki', link: '/wiki'},
     {name: 'Projects', link: '/projects'},
     {name: 'Sponsors', link: '/sponsors'},
     {name: 'Join Us', link: '/join-us'}
